79f3a748835c2babf9bfeedcc4b077315d68aced
Jiwon <jiwon1.han@gmail.com>
2020-05-08 14:45:29 -0700

    practicing react native

--- capstone/src/Weather.js
+++ capstone/src/Weather.js
@@ -4,6 +4,14 @@ import { LinearGradient } from 'expo';
 
 export default class Weather extends Component {
   render() {
-    
+    <LinearGradient color={["#00C6FB", "#005BEA"]} style={styles.container}>
+
+    </LinearGradient>
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  }
+})
