f7b6d580b1740e67e0f8c8126b657ea9ae6f8634
Jiwon <jiwon1.han@gmail.com>
2020-05-08 16:48:07 -0700

    research

--- capstone/src/Weather.js
+++ capstone/src/Weather.js
@@ -1,17 +1,54 @@
 import React , { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { LinearGradient } from 'expo';
+import { LinearGradient } from 'expo-linear-gradient';
 
 export default class Weather extends Component {
   render() {
-    <LinearGradient color={["#00C6FB", "#005BEA"]} style={styles.container}>
-
+    return (
+    <LinearGradient colors={["#00C6FB", "#005BEA"]} style={styles.container}>
+      <View style={styles.upper}>
+        <Text>Icon Here!</Text>
+        <Text style={styles.temp}>35</Text>
+      </View>
+      <View style={styles.lower}>
+        <Text style={styles.title}>Raining</Text>
+        <Text style={styles.subtitle}>For more Information</Text>
+      </View>
     </LinearGradient>
+    )
   }
 }
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1
+  },
+  upper: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  temp: {
+    fontSize: 38,
+    color: "white",
+    marginTop: 10 
+  },
+  lower: {
+    flex: 1,
+    alignItems: "flex-start",
+    justifyContent: "flex-end",
+    paddingLeft: 25,
+  },
+  title: {
+    fontSize: 38,
+    color: "white",
+    marginBottom: 10,
+    fontWeight: "300"
+  },
+  subtitle: {
+    fontSize: 24,
+    color: "white",
+    marginBottom: 24 
   }
-})
+});
