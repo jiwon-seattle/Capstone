87b73be3675f9a014e4c7ca4c570d2c6911fcb61
Jiwon <jiwon1.han@gmail.com>
2020-05-08 19:47:16 -0700

    api work done

--- capstone/src/App.js
+++ capstone/src/App.js
@@ -1,53 +1,62 @@
-import * as React from 'react';
-import { StyleSheet, Text, View, StatusBar } from 'react-native';
+import React, { Component } from "react";
+import { StyleSheet, Text, View, StatusBar } from "react-native";
 import Weather from "./Weather";
 
-const API_KEY= "64f606f4c956d314b74c3fb5e267823a";
+const API_KEY = '64f606f4c956d314b74c3fb5e267823a';
 
-
-
-
-export default class App extends React.Component {
-  state= {
+export default class App extends Component {
+  state = {
     isLoaded: false,
     error: null,
     temperature: null,
-    name: null,
-  }
+    name: null
+  };
   componentDidMount() {
     navigator.geolocation.getCurrentPosition(
       position => {
-        this._getWeather(position.coords.latitude, position.coord.longtitude);
+        this._getWeather(position.coords.latitude, position.coords.longitude);
       },
       error => {
-        this.setState( 
-          {error: error}
-        )
+        this.setState({
+          error: error
+        });
       }
-    )
+    );
   }
-
-_getWeather=(lat, long) => {
-  fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${API_KEY}`).then(reponse => reponse.json()).then(json=> {
-    this.setState({
-      temperature: json.main.temp,
-      name: json.weather[0].main,
-      isLoaded: true,
-
-    })
-  })
+  _getWeather = (lat, long) => {
+    fetch(
+      `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${API_KEY}`
+    )
+      .then(response => response.json())
+      .then(json => {
+        this.setState({
+          isLoaded: true,
+          temperature: json.main.temp,
+          name: json.weather[0].main,
+        });
+      });
+  };
+ render() {
+    const { isLoaded, error, temperature, name } = this.state;
+    return (
+      <View style={styles.container}>
+        <StatusBar hidden={true} />
+        {isLoaded ? (
+          <Weather
+            weatherName={"Mist"}
+            temp={Math.ceil(temperature - 273.15)}
+          />
+        ) : (
+          <View style={styles.loading}>
+            <Text style={styles.loadingText}>Getting the weather</Text>
+            {error ? <Text style={styles.errorText}>{error}</Text> : null}
+          </View>
+        )}
+      </View>
+    );
+  }
 }
 
-render () {
-  const { isLoaded, error } = this.state;
-  return (
-    <View style={styles.container}>
-      <StatusBar hidden={true}/>
-      {isLoaded ? (<Weather/>) : (<View style={styles.loading}><Text style={styles.loadingText}>Getting weather</Text> {error ? <Text style={styles.errorText}>(error)</Text> : null }</View>)}
-    </View>
-  )
-}
-}
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -55,18 +64,17 @@ const styles = StyleSheet.create({
   },
   errorText: {
     color: "red",
-    backgroundColor: "transparent"
+    backgroundColor: "transparent",
+    marginBottom: 40
   },
-  loading:{
+  loading: {
     flex: 1,
     backgroundColor: "#FDF6AA",
-    //bottom 
     justifyContent: "flex-end",
-    paddingLeft: 25,
+    paddingLeft: 25
   },
   loadingText: {
     fontSize: 38,
-    marginBottom: 100
+    marginBottom: 24
   }
-
 });
