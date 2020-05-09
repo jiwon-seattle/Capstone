3151d14088bc38541833cfc2b76863576e263705
Jiwon <jiwon1.han@gmail.com>
2020-05-08 17:04:16 -0700

    icon from expo

--- capstone/src/Weather.js
+++ capstone/src/Weather.js
@@ -1,13 +1,14 @@
 import React , { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
+import { Ionicons } from '@expo/vector-icons'; //importing icons
 
 export default class Weather extends Component {
   render() {
     return (
     <LinearGradient colors={["#00C6FB", "#005BEA"]} style={styles.container}>
       <View style={styles.upper}>
-        <Text>Icon Here!</Text>
+        <Ionicons color="white" size={144} name="ios-rainy"/>
         <Text style={styles.temp}>35</Text>
       </View>
       <View style={styles.lower}>
@@ -28,9 +29,10 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
+    backgroundColor: "transparent"
   },
   temp: {
-    fontSize: 38,
+    fontSize: 48,
     color: "white",
     marginTop: 10 
   },
